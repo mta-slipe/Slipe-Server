699093a7998659a7be7f8a477211539915422c2f
CrosRoad95 <sebajura1234@gmail.com>
2020-06-29 17:31:09 +0200

    refactor to use packetBuilder

--- MtaServer.Packets/Definitions/Lua/Rpc/Camera/SetCameraMatrixPacket.cs
+++ MtaServer.Packets/Definitions/Lua/Rpc/Camera/SetCameraMatrixPacket.cs
@@ -1,25 +1,44 @@
-﻿using System;
+﻿using MtaServer.Packets.Enums;
+using System;
 using System.Linq;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 
 namespace MtaServer.Packets.Lua.Camera
 {
-    public class SetCameraMatrixPacket : LuaPacket
+    public class SetCameraMatrixPacket : Packet
     {
-        public SetCameraMatrixPacket(Vector3 position, Vector3 lookAt, float roll, float fov, byte timeContext) : base(Enums.ElementRPCFunction.SET_CAMERA_MATRIX)
+        public override PacketId PacketId => PacketId.PACKET_ID_LUA;
+        public override PacketFlags Flags => PacketFlags.PACKET_MEDIUM_PRIORITY;
+
+        public Vector3 Position { get; set; }
+        public Vector3 LookAt { get; set; }
+        public float Roll { get; set; }
+        public float Fov { get; set; }
+        public byte TimeContext { get; set; }
+        public SetCameraMatrixPacket(Vector3 position, Vector3 lookAt, float roll, float fov, byte timeContext)
         {
-            Data = Data
-                .Concat(new byte[] { timeContext })
-                .Concat(BitConverter.GetBytes(position.X))
-                .Concat(BitConverter.GetBytes(position.Y))
-                .Concat(BitConverter.GetBytes(position.Z))
-                .Concat(BitConverter.GetBytes(lookAt.X))
-                .Concat(BitConverter.GetBytes(lookAt.Y))
-                .Concat(BitConverter.GetBytes(lookAt.Z))
-                .Concat(BitConverter.GetBytes(roll))
-                .Concat(BitConverter.GetBytes(fov))
-                .ToArray();
+            this.Position = position;
+            this.LookAt = lookAt;
+            this.Roll = roll;
+            this.Fov = fov;
+            this.TimeContext = timeContext;
         }
-    }
+
+        public override void Read(byte[] bytes)
+        {
+        }
+
+        public override byte[] Write()
+        {
+            var builder = new PacketBuilder();
+            builder.Write((byte)ElementRPCFunction.SET_CAMERA_MATRIX);
+            builder.Write(this.TimeContext);
+            builder.Write(this.Position);
+            builder.Write(this.LookAt);
+            builder.Write(this.Roll);
+            builder.Write(this.Fov);
+            return builder.Build();
+        }
+        }
 }
