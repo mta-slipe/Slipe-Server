b71c18ebeebdeb4f5598a799636b688589c647da
CrosRoad95 <sebajura1234@gmail.com>
2020-06-29 16:48:32 +0200

    new packet

---
+++ MtaServer.Packets/Definitions/Player/ClearChatPacket.cs
@@ -0,0 +1,29 @@
+﻿using MtaServer.Packets.Enums;
+using System;
+using System.Drawing;
+using System.Linq;
+
+namespace MtaServer.Packets.Definitions.Player
+{
+    public class ClearChatPacket : Packet
+    {
+        public override PacketId PacketId => PacketId.PACKET_ID_CHAT_CLEAR;
+        public override PacketFlags Flags => PacketFlags.PACKET_MEDIUM_PRIORITY;
+
+        public ClearChatPacket()
+        {
+
+        }
+
+        public override void Read(byte[] bytes)
+        {
+        }
+
+        public override byte[] Write()
+        {
+            var builder = new PacketBuilder();
+            builder.Write(false);
+            return builder.Build();
+        }
+    }
+}
