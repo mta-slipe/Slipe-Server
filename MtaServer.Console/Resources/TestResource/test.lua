7fb36fdd253792f6c40239e53112b94a8a6793b2
Bob <bobvanhooff@outlook.com>
2020-09-15 16:25:09 +0200

    Lua fix for event test

--- MtaServer.Console/Resources/TestResource/test.lua
+++ MtaServer.Console/Resources/TestResource/test.lua
@@ -9,7 +9,7 @@ triggerServerEvent("Slipe.Test.Event", root, "String value", true, 123, {
 
 addEvent("Slipe.Test.ClientEvent", true)
 addEventHandler("Slipe.Test.ClientEvent", root, function(...) 
-	for k, v in pairs(...) do
+	for k, v in pairs({...}) do
 		iprint(k, v)
 	end
 end)
