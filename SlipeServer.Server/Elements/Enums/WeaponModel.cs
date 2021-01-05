5132b8dce96e67275fabf28baa556f191f059abe
Bob <bobvanhooff@outlook.com>
2021-01-05 20:28:55 +0100

    Weapon model enum

---
+++ SlipeServer.Server/Elements/Enums/WeaponModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlipeServer.Server.Elements.Enums
+{
+    public enum WeaponModel
+    {
+        WEAPONTYPE_UNARMED = 0,
+        Brassknuckle,
+        Golfclub,
+        Nightstick,
+        Knife,
+        BaseballBat,
+        Shovel,
+        PoolCue,
+        Katana,
+        Chainsaw,
+
+        Dildo1,
+        Dildo2,
+        Vibrator1,
+        Vibrator2,
+        Flowers,
+        Cane,
+
+        Grenade,
+        Teargass,
+        Molotov,
+        RpgMissile,
+        HeatSeekingRpgMissile,
+        FreefallBomb,
+
+        Colt,
+        Silenced,
+        DesertEagle,
+        Shotgun,
+        Sawnoff,
+        CombatShotgun,
+        Uzi,
+        Mp5,
+
+        Ak47,
+        M4,
+
+        Tec9,
+
+        Rifle,
+        SniperRifle,
+        Rpg,
+        HeatSeekingRpg,
+        Flamethrower,
+        Minigun,
+        Sathcel,
+        Detonator,
+        Spraycan,
+        FireExtinguisher,
+        Camera,
+        NightVissionGoggles,
+        InfraredGoggles,
+        Parachute,
+    }
+}
