128b98f09a7865ae70445d0f8443370881c0ce75
_ERAGON_ <turk6486@gmail.com>
2021-07-17 21:16:59 +0430

    Add VoiceDataPacket and VoiceEndPacket definitions

---
+++ SlipeServer.Packets/Definitions/Voice/VoiceDataPacket.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.Serialization;
+using SlipeServer.Packets.Builder;
+using SlipeServer.Packets.Enums;
+using SlipeServer.Packets.Reader;
+using System.Text;
+
+namespace SlipeServer.Packets.Definitions.Voice
+{
+    public class VoiceDataPacket : Packet
+    {
+        public override PacketId PacketId { get; } = PacketId.PACKET_ID_VOICE_DATA;
+        public override PacketReliability Reliability { get; } = PacketReliability.ReliableSequenced;
+        public override PacketPriority Priority { get; } = PacketPriority.Low;
+
+        public byte[]? Buffer { get; set; } = null;
+        public uint SourceElementId { get; set; }
+
+        public VoiceDataPacket(in uint elementId, in byte[] sourceBuffer, ushort bufferSize)
+        {
+            this.Buffer = new byte[bufferSize];
+
+            this.Buffer = sourceBuffer;
+            
+            this.SourceElementId = elementId;
+        }
+        
+        public override byte[] Write()
+        {
+            var builder = new PacketBuilder();
+
+            // Write the source player id
+            builder.WriteElementId(this.SourceElementId);
+            // Write the length as an unsigned short integer
+            builder.Write((ushort)this.Buffer!.Length);
+            // Write the string data
+            builder.Write(Encoding.Default.GetString(this.Buffer));
+
+            return builder.Build();
+        }
+
+        public override void Read(byte[] bytes)
+        {
+            if (this.Buffer != null)
+            {
+                var reader = new PacketReader(bytes);
+
+                this.Buffer = reader.GetBytes(bytes.Length);
+            }
+        }
+    }
+}
