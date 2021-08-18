3c2a37f79164e7eb8b4fd67e854e580c33f9f60f
NanoBob <info@nanobob.net>
2021-08-18 21:11:13 +0100

    Server builder with default option

---
+++ SlipeServer.Server/ServerOptions/ServerBuildStep.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SlipeServer.Server.ServerOptions
+{
+    public struct ServerBuildStep
+    {
+        public Action<MtaServer> Step { get; init; }
+        public ServerBuildStepPriority Priority { get; init; }
+
+        public ServerBuildStep(Action<MtaServer> step, ServerBuildStepPriority priority)
+        {
+            this.Step = step;
+            this.Priority = priority;
+        }
+    }
+}
