d2a2779232f9672dcbcc899f30374e52bb9a7315
CrosRoad95 <sebajura1234@gmail.com>
2021-08-19 17:59:14 +0200

    GlitchType

---
+++ SlipeServer.Packets/Enums/GlitchType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlipeServer.Packets.Enums
+{
+    public enum GlitchType
+    {
+        GLITCH_QUICKRELOAD,
+        GLITCH_FASTFIRE,
+        GLITCH_FASTMOVE,
+        GLITCH_CROUCHBUG,
+        GLITCH_CLOSEDAMAGE,
+        GLITCH_HITANIM,
+        GLITCH_FASTSPRINT,
+        GLITCH_BADDRIVEBYHITBOX,
+        GLITCH_QUICKSTAND,
+        GLITCH_KICKOUTOFVEHICLE_ONMODELREPLACE,
+    };
+}
