f65a6d3e319154216226320fd30e917e4f9e69c1
CrosRoad95 <sebajura1234@gmail.com>
2021-08-20 14:50:49 +0200

    remove test code

--- SlipeServer.Scripting/ScriptInputRuntime.cs
+++ SlipeServer.Scripting/ScriptInputRuntime.cs
@@ -58,19 +58,7 @@ namespace SlipeServer.Scripting
             if (callbackDelegate == null)
                 this.registeredCommandHandlers.RemoveAll(x => x.CommandName == commandName);
             else
-                this.registeredCommandHandlers.RemoveAll(x =>
-                {
-                    var x1 = callbackDelegate.Target;
-                    var y2 = x.Delegate.Target;
-                    var foo = x1 == y2;
-                    var asd = x.Delegate == callbackDelegate;
-                    var asd2 = x.Delegate.Equals(callbackDelegate);
-                    if(foo || asd || asd2)
-                    {
-                        var asdasd = 5;
-                    }
-                    return x.CommandName == commandName && asd;
-                });
+                this.registeredCommandHandlers.RemoveAll(x => x.CommandName == commandName && x.Delegate == callbackDelegate);
         }
     }
 
