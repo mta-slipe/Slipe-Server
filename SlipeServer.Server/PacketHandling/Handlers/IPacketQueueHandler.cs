ff0cbcddce7e72330fd8f18997a2b40a1b7f9fb6
NanoBob <info@nanobob.net>
2021-09-12 19:48:30 +0100

    Return packet to packet pool after handling, not after enqueueing

--- SlipeServer.Server/PacketHandling/Handlers/IPacketQueueHandler.cs
+++ SlipeServer.Server/PacketHandling/Handlers/IPacketQueueHandler.cs
@@ -1,9 +1,12 @@
 ﻿using SlipeServer.Packets;
+using System;
 
 namespace SlipeServer.Server.PacketHandling.Handlers
 {
     public interface IPacketQueueHandler<T> where T : Packet
     {
         void EnqueuePacket(Client client, T packet);
+
+        event Action<T> PacketHandled;
     }
 }
