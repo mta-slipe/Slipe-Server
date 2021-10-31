a31be49475256337549e7f91f40553b788424fa1
NanoBob <info@nanobob.net>
2021-10-31 22:28:40 +0000

    Source generator for lua event parsing, some fixes for lua events

---
+++ SlipeServer.Console/LuaEvents/SampleLuaEvent.cs
@@ -0,0 +1,31 @@
+﻿using SlipeServer.Server.Events;
+using SlipeServer.SourceGenerators;
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace SlipeServer.Console.LuaEvents
+{
+    [LuaEvent]
+    public partial class SampleLuaEvent
+    {
+        public float Float { get; set; }
+        public float? OptionalFloat { get; set; }
+        public double Double { get; set; }
+        public double? OptionalDouble { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3? OptionalPosition { get; set; }
+        public int Integer { get; set; }
+        public int? OptionalInteger { get; set; }
+
+        [Required]
+        public string Text { get; set; } = null!;
+
+        [Required]
+        public string Text2 { get; set; } = null!;
+
+        public bool Boolean { get; set; }
+        public bool? OptionalBoolean { get; set; }
+
+        public partial void Parse(LuaEvent luaEvent);
+    }
+}
---
+++ SlipeServer.SourceGenerators/LuaEventAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlipeServer.SourceGenerators
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = true)]
+    public sealed class LuaEventAttribute : Attribute
+    {
+        public LuaEventAttribute()
+        {
+        }
+    }
+}
