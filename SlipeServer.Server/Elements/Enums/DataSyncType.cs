ac10dc1711db585de487733c8efe8eceb5d67541
_ERAGON_ <turk6486@gmail.com>
2021-11-08 17:44:31 +0330

    Add data sync type enum

---
+++ SlipeServer.Server/Elements/Enums/DataSyncType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlipeServer.Server.Elements.Enums
+{
+    public enum DataSyncType
+    {
+        Local,
+        Broadcast,
+        Private
+    }
+}
