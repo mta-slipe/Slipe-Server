1132a768e8567ae47b88f8f3393478754f7541e7
NanoBob <info@nanobob.net>
2022-03-20 20:54:14 +0000

    Unoccupied vehicle sync, and vehicle push sync

---
+++ SlipeServer.Server/Elements/Events/VehiclePushedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SlipeServer.Server.Elements.Events
+{
+    public class VehiclePushedEventArgs : EventArgs
+    {
+        public Player Pusher { get; }
+
+        public VehiclePushedEventArgs(Player pusher)
+        {
+            this.Pusher = pusher;
+        }
+    }
+}
