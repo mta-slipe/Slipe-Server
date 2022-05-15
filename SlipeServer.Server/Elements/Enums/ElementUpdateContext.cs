748a9efbd108e63789333b72a693d273f46bbb30
NanoBob <info@nanobob.net>
2022-05-15 14:00:57 +0100

    Swap default

--- SlipeServer.Server/Elements/Enums/ElementUpdateContext.cs
+++ SlipeServer.Server/Elements/Enums/ElementUpdateContext.cs
@@ -5,8 +5,8 @@ namespace SlipeServer.Server.Elements.Enums;
 [Flags]
 public enum ElementUpdateContext
 {
-    NoRelay = 0x00,
-    Relay = 0x01,
+    Relay = 0x00,
+    NoRelay = 0x01,
     ServerOnly = 0x02 | NoRelay,
     PostEvent = 0x04 | Relay,
 
