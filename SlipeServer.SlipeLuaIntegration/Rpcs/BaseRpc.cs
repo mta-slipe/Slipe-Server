1157f403b5b0a3e687e16cde7489e2bccbf0ca1f
NanoBob <info@nanobob.net>
2022-10-16 20:19:52 +0100

    Commented out WIP stuff

--- SlipeServer.SlipeLuaIntegration/Rpcs/BaseRpc.cs
+++ SlipeServer.SlipeLuaIntegration/Rpcs/BaseRpc.cs
@@ -1,50 +1,50 @@
-﻿using SlipeLua.Shared.Rpc;
+﻿//using SlipeLua.Shared.Rpc;
 
-namespace SlipeServer.SlipeLuaIntegration.Rpcs;
+//namespace SlipeServer.SlipeLuaIntegration.Rpcs;
 
-public abstract class BaseRpc : IRpc
-{
-    public ClientRpcFailedAction OnClientRpcFailed { get; set; }
+//public abstract class BaseRpc : IRpc
+//{
+//    public ClientRpcFailedAction OnClientRpcFailed { get; set; }
 
-    public BaseRpc()
-    {
-        this.OnClientRpcFailed = ClientRpcFailedAction.Ignore;
-    }
+//    public BaseRpc()
+//    {
+//        this.OnClientRpcFailed = ClientRpcFailedAction.Ignore;
+//    }
 
-    protected T[] GetArray<T>(dynamic table)
-    {
-        return SlipeLua.MtaDefinitions.MtaShared.GetArrayFromTable<T>(table, "");
-    }
+//    protected T[] GetArray<T>(dynamic table)
+//    {
+//        return SlipeLua.MtaDefinitions.MtaShared.GetArrayFromTable<T>(table, "");
+//    }
 
-    protected MtaElement[] CreateElementArray(Element[] elements)
-    {
-        MtaElement[] mtaElements = new MtaElement[elements.Length];
+//    protected MtaElement[] CreateElementArray(Element[] elements)
+//    {
+//        MtaElement[] mtaElements = new MtaElement[elements.Length];
 
-        for (int i = 0; i < elements.Length; i++)
-        {
-            mtaElements[i] = elements[i].MTAElement;
-        }
+//        for (int i = 0; i < elements.Length; i++)
+//        {
+//            mtaElements[i] = elements[i].MTAElement;
+//        }
 
-        return mtaElements;
-    }
+//        return mtaElements;
+//    }
 
-    protected T[] GetElementArray<T>(dynamic table) where T : Element
-    {
-        dynamic[] mtaElements = Slipe.MtaDefinitions.MtaShared.GetArrayFromTable<MtaElement>(table, "");
+//    protected T[] GetElementArray<T>(dynamic table) where T : Element
+//    {
+//        dynamic[] mtaElements = Slipe.MtaDefinitions.MtaShared.GetArrayFromTable<MtaElement>(table, "");
 
-        T[] elements = new T[mtaElements.Length];
-        for (int i = 0; i < mtaElements.Length; i++)
-        {
-            elements[i] = GetElement<T>(mtaElements[i]);
-        }
-        return elements;
-    }
+//        T[] elements = new T[mtaElements.Length];
+//        for (int i = 0; i < mtaElements.Length; i++)
+//        {
+//            elements[i] = GetElement<T>(mtaElements[i]);
+//        }
+//        return elements;
+//    }
 
-    protected T GetElement<T>(dynamic mtaElement) where T : Element
-    {
-        return ElementManager.Instance.GetElement<T>(mtaElement.element);
-    }
+//    protected T GetElement<T>(dynamic mtaElement) where T : Element
+//    {
+//        return ElementManager.Instance.GetElement<T>(mtaElement.element);
+//    }
 
-    public abstract void Parse(dynamic value);
+//    public abstract void Parse(dynamic value);
 
-}
+//}
