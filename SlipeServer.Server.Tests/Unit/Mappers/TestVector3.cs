1bf89ebf82a98ffbc3b06b736e8754635ec1948b
Bob <bobvanhooff@outlook.com>
2022-10-25 21:16:12 +0200

    Lua mapper tests and fixes

---
+++ SlipeServer.Server.Tests/Unit/Mappers/TestVector3.cs
@@ -0,0 +1,15 @@
+﻿namespace SlipeServer.Server.Tests.Unit.Mappers;
+
+internal struct TestVector3
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+
+    public TestVector3(float x, float y, float z)
+    {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+    }
+}
