fe6e79ef61d084ed8efb6345e0c7c9f304d1ab70
Bob van Hooff <b.van.hooff@betabit.nl>
2022-11-01 21:02:08 +0100

    Test default weapon stats loading

---
+++ SlipeServer.Server.Tests/Unit/Services/WeaponConfigurationServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using SlipeServer.Server.Constants;
+using SlipeServer.Server.Services;
+using SlipeServer.Server.TestTools;
+using System;
+using Xunit;
+
+namespace SlipeServer.Server.Tests.Unit.Services;
+
+public class WeaponConfigurationServiceTests
+{
+    [Fact]
+    public void GetWeaponConfiguration_ReturnsDefaults_WhenUnmodified()
+    {
+        var mtaServer = new TestingServer();
+
+        var service = new WeaponConfigurationService(mtaServer);
+
+        foreach (var weapon in Enum.GetValues<Enums.WeaponId>())
+        {
+            foreach (var level in Enum.GetValues<Enums.WeaponSkillLevel>())
+            {
+                var result = service.GetWeaponConfiguration(weapon, level);
+
+                if (WeaponConfigurationConstants.Defaults.ContainsKey(weapon) && WeaponConfigurationConstants.Defaults[weapon].ContainsKey(level))
+                    result.Should().BeEquivalentTo(WeaponConfigurationConstants.Defaults[weapon][level]);
+            }
+        }
+    }
+}
