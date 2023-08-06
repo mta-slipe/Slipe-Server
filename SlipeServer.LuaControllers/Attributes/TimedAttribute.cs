ca4273409d2cc009a6c75cf149f61e36e754f9ee
NanoBob <info@nanobob.net>
2023-08-06 11:03:08 +0200

    Timed controller methods

---
+++ SlipeServer.LuaControllers/Attributes/TimedAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace SlipeServer.LuaControllers.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class TimedAttribute : Attribute
+{
+    public TimeSpan Interval { get; }
+
+    public TimedAttribute(int intervalInMilliseconds)
+    {
+        this.Interval = TimeSpan.FromMilliseconds(intervalInMilliseconds);
+    }
+
+    public TimedAttribute(int hours, int minutes, int seconds)
+    {
+        this.Interval = new TimeSpan(hours, minutes, seconds);
+    }
+
+    public TimedAttribute(string timespan)
+    {
+        this.Interval = TimeSpan.Parse(timespan);
+    }
+}
