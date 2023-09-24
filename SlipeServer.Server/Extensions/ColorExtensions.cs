034b20fcc86ae573b14dacbc6971a337d827d5c7
Sebastian Jura <sebajura1234@gmail.com>
2023-09-24 08:20:00 +0200

    add ToLuaColor extension for color

--- SlipeServer.Server/Extensions/ColorExtensions.cs
+++ SlipeServer.Server/Extensions/ColorExtensions.cs
@@ -14,4 +14,9 @@ public static class ColorExtensions
 
         return $"#{color.Value.R:X2}{color.Value.G:X2}{color.Value.B:X2}";
     }
+
+    /// <summary>
+    /// Returns the retrieves the hex number of a specified color, useful for the dx functions. Equivalent of lua tocolor function
+    /// </summary>
+    public static double ToLuaColor(this Color color) => color.B + color.G * 256 + color.R * 256 * 256 + color.A * 256 * 256 * 256;
 }
