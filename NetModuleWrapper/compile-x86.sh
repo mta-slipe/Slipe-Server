d6ca06e3b5724372f543ab49797c0bf189a0bf41
Bob <bobvanhooff@outlook.com>
2023-12-02 23:54:07 +0100

    Attempt 2 at running on ARM

---
+++ NetModuleWrapper/compile-x86.sh
@@ -0,0 +1,18 @@
+sudo apt-get install g++-multilib gcc-multilib -y
+sudo apt-get install linux-libc-dev linux-libc-dev:i386
+
+mkdir -p x64
+g++ -c -o x64/wrapperFunctions.o wrapperFunctions.cpp -fPIC
+g++ -c -o x64/NetWrapper.o NetWrapper.cpp -fPIC
+g++ -c -o x64/CDynamicLibrary.o mta/core/CDynamicLibrary.cpp -fPIC
+gcc x64/wrapperFunctions.o x64/NetWrapper.o x64/CDynamicLibrary.o -shared -o x64/NetModuleWrapper.so
+
+cp x64/NetModuleWrapper.so ../SlipeServer.Net/Binaries/x64/
+
+mkdir -p x86
+g++ -c -m32 -o x86/wrapperFunctions.o wrapperFunctions.cpp -fPIC
+g++ -c -m32 -o x86/NetWrapper.o NetWrapper.cpp -fPIC
+g++ -c -m32 -o x86/CDynamicLibrary.o mta/core/CDynamicLibrary.cpp -fPIC
+gcc -m32 x86/wrapperFunctions.o x86/NetWrapper.o x86/CDynamicLibrary.o -shared -o x86/NetModuleWrapper.so
+
+cp x86/NetModuleWrapper.so ../SlipeServer.Net/Binaries/x86/
--- NetModuleWrapper/compile.sh
+++ NetModuleWrapper/compile.sh
@@ -1,16 +1,4 @@
-mkdir -p x64
-g++ -c -o x64/wrapperFunctions.o wrapperFunctions.cpp -fPIC
-g++ -c -o x64/NetWrapper.o NetWrapper.cpp -fPIC
-g++ -c -o x64/CDynamicLibrary.o mta/core/CDynamicLibrary.cpp -fPIC
-gcc x64/wrapperFunctions.o x64/NetWrapper.o x64/CDynamicLibrary.o -shared -o x64/NetModuleWrapper.so
+sudo apt-get update
 
-cp x64/NetModuleWrapper.so ../SlipeServer.Net/Binaries/x64/
-
-mkdir -p x86
-g++ -c -m32 -o x86/wrapperFunctions.o wrapperFunctions.cpp -fPIC
-g++ -c -m32 -o x86/NetWrapper.o NetWrapper.cpp -fPIC
-g++ -c -m32 -o x86/CDynamicLibrary.o mta/core/CDynamicLibrary.cpp -fPIC
-gcc -m32 x86/wrapperFunctions.o x86/NetWrapper.o x86/CDynamicLibrary.o -shared -o x86/NetModuleWrapper.so
-
-cp x86/NetModuleWrapper.so ../SlipeServer.Net/Binaries/x86/
-
+./compile-arm.sh
+./compile-x86.sh
