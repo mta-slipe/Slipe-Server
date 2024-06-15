b3e38ff552e2dae084397d4830cdd2d9d9a85ec6
Bob <bobvanhooff@outlook.com>
2024-06-15 18:41:43 +0200

    Overhauled hostbuilder, fixed regular usage

---
+++ SlipeServer.Hosting/MtaServerHostedService.cs
@@ -0,0 +1,27 @@
+﻿namespace SlipeServer.Hosting;
+
+public class MtaServerHostedService<T> : IHostedService where T : MtaServer
+{
+    private readonly T server;
+
+    public MtaServerHostedService(T server)
+    {
+        this.server = server;
+
+        server.BuildFinalizer?.Invoke();
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        this.server.Start();
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        this.server.Stop();
+
+        return Task.CompletedTask;
+    }
+}
