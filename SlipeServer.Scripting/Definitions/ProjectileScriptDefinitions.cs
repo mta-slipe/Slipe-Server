ddc69f89ae5403cda822aca396a19ad59ad7d141
Sebastian Jura <22455534+CrosRoad95@users.noreply.github.com>
2024-10-19 16:53:21 +0200

    Tweak vehicle damage logic, add more test and  test commands

---
+++ SlipeServer.Scripting/Definitions/ProjectileScriptDefinitions.cs
@@ -0,0 +1,16 @@
+﻿using SlipeServer.Server.Elements;
+
+namespace SlipeServer.Scripting.Definitions;
+
+public class ProjectileScriptDefinitions
+{
+    public ProjectileScriptDefinitions()
+    {
+    }
+
+    [ScriptFunctionDefinition("detonateSatchels")]
+    public void DetonateSatchels(Player player)
+    {
+
+    }
+}
