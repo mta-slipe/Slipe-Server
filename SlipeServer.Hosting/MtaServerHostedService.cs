a2a34a3a2ef9d31fba89e30f930039b207aba291
Sebastian Jura <22455534+CrosRoad95@users.noreply.github.com>
2024-10-29 06:56:51 +0100

    Improve MtaServerHostedService

--- SlipeServer.Hosting/MtaServerHostedService.cs
+++ SlipeServer.Hosting/MtaServerHostedService.cs
@@ -1,6 +1,6 @@
 ﻿namespace SlipeServer.Hosting;
 
-public class MtaServerHostedService<T> : IHostedService where T : MtaServer
+public class MtaServerHostedService<T> : IHostedLifecycleService where T : MtaServer
 {
     private readonly T server;
 
@@ -8,17 +8,37 @@ public class MtaServerHostedService<T> : IHostedService where T : MtaServer
     {
         this.server = server;
 
-        server.BuildFinalizer?.Invoke();
+        this.server.BuildFinalizer?.Invoke();
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StartedAsync(CancellationToken cancellationToken)
     {
         this.server.Start();
 
         return Task.CompletedTask;
     }
 
+    public Task StartingAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
     public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StoppedAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StoppingAsync(CancellationToken cancellationToken)
     {
         this.server.Stop();
 
